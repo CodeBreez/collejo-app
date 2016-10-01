21df4c2a4fe21133f3c0675f3d7a8560ea18c4b6
Anuradha Jayathilaka <astroanu2004@gmail.com>
2016-10-01 20:20:12 +0000

    added user permission listeners

---
+++ src/Listeners/UserPermissionsAlertEmail.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace Collejo\App\Listeners;
+
+use Collejo\App\Events\UserPermissionsChanged;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Cache;
+
+class UserPermissionsAlertEmail implements ShouldQueue{
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct() {
+
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserPermissionsChanged  $event
+     * @return void
+     */
+    public function handle(UserPermissionsChanged $event)
+    {
+        // email all users
+    }
+}
