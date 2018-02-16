451a479dce634a3b5ecb2144aa0be3d64bccc53b
Anuradha <astroanu2004@gmail.com>
2018-02-16 22:49:53 +0530

    Factory change params

--- App/Modules/ACL/Models/factories/Permission.php
+++ App/Modules/ACL/Models/factories/Permission.php
@@ -3,6 +3,6 @@
 $factory->define(Collejo\App\Modules\ACL\Models\Permission::class, function (Faker\Generator $faker) {
     return [
         'module'           => 'test',
-        'permission'       => $faker->randomLetter.$faker->randomDigit,
+        'permission'       => $faker->randomLetter.$faker->time('H:i:s'),
     ];
 });
--- App/Modules/ACL/Models/factories/Role.php
+++ App/Modules/ACL/Models/factories/Role.php
@@ -2,6 +2,6 @@
 
 $factory->define(Collejo\App\Modules\ACL\Models\Role::class, function (Faker\Generator $faker) {
     return [
-        'role'           => $faker->randomLetter.$faker->randomDigit,
+        'role'           => $faker->randomLetter.$faker->time('H:i:s'),
     ];
 });
