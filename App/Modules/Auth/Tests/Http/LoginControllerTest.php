17e89707a50a767328ff712ab6aa95915d94c6e2
astroanu <astroanu2004@gmail.com>
2018-02-24 17:48:17 +0000

    Apply fixes from StyleCI

--- App/Modules/Auth/Tests/Http/LoginControllerTest.php
+++ App/Modules/Auth/Tests/Http/LoginControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace Collejo\App\Modules\Auth\Tests\Browser;
 
-use Collejo\Foundation\Testing\TestCase;
 use Collejo\App\Modules\ACL\Models\User;
+use Collejo\Foundation\Testing\TestCase;
 
 class LoginControllerTest extends TestCase
 {
@@ -31,8 +31,8 @@ class LoginControllerTest extends TestCase
         $user = factory(User::class)->create();
 
         $credentials = [
-            'email' => $user->email,
-            'password' => 123
+            'email'    => $user->email,
+            'password' => 123,
         ];
 
         $response = $this->post(route('login'), $credentials);
@@ -53,8 +53,8 @@ class LoginControllerTest extends TestCase
         $user = factory(User::class)->create();
 
         $credentials = [
-            'email' => $user->email,
-            'password' => 'wrong password'
+            'email'    => $user->email,
+            'password' => 'wrong password',
         ];
 
         $response = $this->post(route('login'), $credentials);
