976089db392083cddf5d4714ddd6962af57d9332
astroanu <astroanu2004@gmail.com>
2018-02-25 15:23:14 +0000

    Apply fixes from StyleCI

--- Foundation/Repository/Tests/RepositoryTest.php
+++ Foundation/Repository/Tests/RepositoryTest.php
@@ -2,27 +2,26 @@
 
 namespace Collejo\Foundation\Repository\Tests;
 
-use Collejo\Foundation\Testing\TestCase;
 use Collejo\Foundation\Repository\BaseRepository;
+use Collejo\Foundation\Testing\TestCase;
 use Uuid;
 
 class RepositoryTest extends TestCase
 {
-
     /**
-     * Test if a new UUID could be generated
+     * Test if a new UUID could be generated.
      */
     public function testNewUuid()
     {
         $uuid = $this->getRepository()->newUuid();
 
         $this->assertTrue(is_string($uuid));
-        $this->assertEquals(strlen((string)$uuid), 36);
-        $this->assertTrue(Uuid::validate((string)$uuid));
+        $this->assertEquals(strlen((string) $uuid), 36);
+        $this->assertTrue(Uuid::validate((string) $uuid));
     }
 
     /**
-     * Returns a new instance of the TestableRepository
+     * Returns a new instance of the TestableRepository.
      *
      * @return TestableRepository
      */
@@ -34,5 +33,4 @@ class RepositoryTest extends TestCase
 
 class TestableRepository extends BaseRepository
 {
-
 }
