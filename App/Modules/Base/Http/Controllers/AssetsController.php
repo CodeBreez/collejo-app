2d3c9ae53db29ca43dba46d0f2f04857047b3cf7
Anuradha <astroanu2004@gmail.com>
2018-03-06 23:24:06 +0530

    Set correct header

--- App/Modules/Base/Http/Controllers/AssetsController.php
+++ App/Modules/Base/Http/Controllers/AssetsController.php
@@ -16,7 +16,8 @@ class AssetsController extends Controller
     public function getLocals()
     {
         $langs = Cache::remember('lang-files', config('languages_cache_ttl'), function () {
-            $langs = ['aa' => [], 'ab' => [], 'ae' => [], 'af' => [], 'ak' => [], 'am' => [], 'an' => [], 'ar' => [], 'as' => [], 'av' => [], 'ay' => [], 'az' => [], 'ba' => [], 'be' => [], 'bg' => [], 'bh' => [], 'bi' => [], 'bm' => [], 'bn' => [], 'bo' => [], 'br' => [], 'bs' => [], 'ca' => [], 'ce' => [], 'ch' => [], 'co' => [], 'cr' => [], 'cs' => [], 'cu' => [], 'cv' => [], 'cy' => [], 'da' => [], 'de' => [], 'dv' => [], 'dz' => [], 'ee' => [], 'el' => [], 'en' => [], 'eo' => [], 'es' => [], 'et' => [], 'eu' => [], 'fa' => [], 'ff' => [], 'fi' => [], 'fj' => [], 'fo' => [], 'fr' => [], 'fy' => [], 'ga' => [], 'gd' => [], 'gl' => [], 'gn' => [], 'gu' => [], 'gv' => [], 'ha' => [], 'he' => [], 'hi' => [], 'ho' => [], 'hr' => [], 'ht' => [], 'hu' => [], 'hy' => [], 'hz' => [], 'ia' => [], 'id' => [], 'ie' => [], 'ig' => [], 'ii' => [], 'ik' => [], 'io' => [], 'is' => [], 'it' => [], 'iu' => [], 'ja' => [], 'jv' => [], 'ka' => [], 'kg' => [], 'ki' => [], 'kj' => [], 'kk' => [], 'kl' => [], 'km' => [], 'kn' => [], 'ko' => [], 'kr' => [], 'ks' => [], 'ku' => [], 'kv' => [], 'kw' => [], 'ky' => [], 'la' => [], 'lb' => [], 'lg' => [], 'li' => [], 'ln' => [], 'lo' => [], 'lt' => [], 'lu' => [], 'lv' => [], 'mg' => [], 'mh' => [], 'mi' => [], 'mk' => [], 'ml' => [], 'mn' => [], 'mr' => [], 'ms' => [], 'mt' => [], 'my' => [], 'na' => [], 'nb' => [], 'nd' => [], 'ne' => [], 'ng' => [], 'nl' => [], 'nn' => [], 'no' => [], 'nr' => [], 'nv' => [], 'ny' => [], 'oc' => [], 'oj' => [], 'om' => [], 'or' => [], 'os' => [], 'pa' => [], 'pi' => [], 'pl' => [], 'ps' => [], 'pt' => [], 'qu' => [], 'rm' => [], 'rn' => [], 'ro' => [], 'ru' => [], 'rw' => [], 'sa' => [], 'sc' => [], 'sd' => [], 'se' => [], 'sg' => [], 'si' => [], 'sk' => [], 'sl' => [], 'sm' => [], 'sn' => [], 'so' => [], 'sq' => [], 'sr' => [], 'ss' => [], 'st' => [], 'su' => [], 'sv' => [], 'sw' => [], 'ta' => [], 'te' => [], 'tg' => [], 'th' => [], 'ti' => [], 'tk' => [], 'tl' => [], 'tn' => [], 'to' => [], 'tr' => [], 'ts' => [], 'tt' => [], 'tw' => [], 'ty' => [], 'ug' => [], 'uk' => [], 'ur' => [], 'uz' => [], 've' => [], 'vi' => [], 'vo' => [], 'wa' => [], 'wo' => [], 'xh' => [], 'yi' => [], 'yo' => [], 'za' => [], 'zh' => [], 'zu' => []];
+
+            $langs = array_fill_keys(config('collejo.languages'), []);
 
             foreach (Module::getModulePaths() as $path) {
                 if (file_exists($path)) {
@@ -42,6 +43,6 @@ class AssetsController extends Controller
         });
 
         return response('C.langs='.json_encode($langs))
-            ->header('Content-Type:', 'application/javascript');
+            ->header('Content-Type', 'application/javascript');
     }
 }
