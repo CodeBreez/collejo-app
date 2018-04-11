8e485d7f655fa5363346196115aebf4daba7cd0b
astroanu <astroanu2004@gmail.com>
2018-04-11 17:16:13 +0000

    Apply fixes from StyleCI

--- App/Modules/ACL/resources/lang/en/user.php
+++ App/Modules/ACL/resources/lang/en/user.php
@@ -4,18 +4,18 @@ return [
     'users'    => 'Users',
     'new_user' => 'New User',
 
-    'email'         => 'Email',
+    'email'               => 'Email',
     'first_name'          => 'First Name',
-    'last_name'     => 'Last Name',
-    'name'=>    'Name',
-    'password' => 'Password',
+    'last_name'           => 'Last Name',
+    'name'                => 'Name',
+    'password'            => 'Password',
 
     'date_of_birth' => 'Date of Birth',
 
     'user_details' => 'User Details',
-    'edit_user' => 'Edit User Details',
+    'edit_user'    => 'Edit User Details',
 
     'empty_list' => 'There are no Users in the system',
 
-    'user_updated' => 'User details updated.'
+    'user_updated' => 'User details updated.',
 ];
