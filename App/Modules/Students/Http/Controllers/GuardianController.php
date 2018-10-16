9d6dcf0ae83dc20557723d260e432c5a3ea65b18
astroanu <astroanu2004@gmail.com>
2018-10-16 17:55:12 +0000

    Apply fixes from StyleCI

--- App/Modules/Students/Http/Controllers/GuardianController.php
+++ App/Modules/Students/Http/Controllers/GuardianController.php
@@ -5,25 +5,26 @@ namespace Collejo\App\Modules\Students\Http\Controllers;
 use Collejo\App\Http\Controller;
 use Collejo\App\Modules\Students\Contracts\GuardianRepository;
 use Collejo\App\Modules\Students\Criteria\GuardiansListCriteria;
-use Request;
 
-class StudentController extends Controller
+class GuardianController extends Controller
 {
     protected $guardianRepository;
 
     /**
      * @param GuardiansListCriteria $criteria
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
      * @throws \Illuminate\Auth\Access\AuthorizationException
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function getGuardiansList(GuardiansListCriteria $criteria)
     {
         $this->authorize('list_guardians');
 
         return view('students::guardians_list', [
-            'criteria' => $criteria,
+            'criteria'  => $criteria,
             'guardians' => $this->guardianRepository->getGuardians($criteria)->with('user', 'students')
-                ->paginate(config('collejo.pagination.perpage'))
+                ->paginate(config('collejo.pagination.perpage')),
         ]);
     }
 
--- App/Modules/Students/Repositories/GuardianRepository.php
+++ App/Modules/Students/Repositories/GuardianRepository.php
@@ -5,11 +5,9 @@ namespace Collejo\App\Modules\Students\Repositories;
 use Collejo\App\Modules\ACL\Contracts\UserRepository;
 use Collejo\App\Modules\Students\Contracts\GuardianRepository as GuardianRepositoryContract;
 use Collejo\Foundation\Repository\BaseRepository;
-use DB;
 
 class GuardianRepository extends BaseRepository implements GuardianRepositoryContract
 {
-
     public function getGuardians($criteria)
     {
         return $this->search($criteria);
