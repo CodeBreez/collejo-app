70b43f2b9fb18e8a174bba9d3d0f1edc79c815ab
astroanu <astroanu2004@gmail.com>
2018-11-17 20:24:39 +0530

    Apply fixes from StyleCI (#58)

--- App/Modules/Students/resources/lang/en/student_category.php
+++ App/Modules/Students/resources/lang/en/student_category.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-	'student_categories' => 'Student Categories',
-	'new_student_category' => 'New Student Category',
-	'name' => 'Name',
-	'code' => 'Code',
+    'student_categories'   => 'Student Categories',
+    'new_student_category' => 'New Student Category',
+    'name'                 => 'Name',
+    'code'                 => 'Code',
 
-	'view_students' => 'View Students',
+    'view_students' => 'View Students',
 
-	'empty_list' => 'There are no Student Categories in the system.',
-	'student_category_created' => 'Student Category created.',
-	'student_category_updated' => 'Student Category updated.',
+    'empty_list'               => 'There are no Student Categories in the system.',
+    'student_category_created' => 'Student Category created.',
+    'student_category_updated' => 'Student Category updated.',
 ];
